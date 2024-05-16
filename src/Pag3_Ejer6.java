0b79dae696b23f25c806abda5d52e9928b78ae2d
Rolando Stiven Flamenco Guerra <redloreheart@gmail.com>
2024-05-16 10:11:22 -0600

    Ejercicio terminado

--- src/Pag3_Ejer6.java
+++ src/Pag3_Ejer6.java
@@ -1,45 +1,16 @@
 import java.util.Scanner;
 
 public class Pag3_Ejer6 {
-     public static void main(String[] args) throws Exception {
-        Scanner teclado = new Scanner(System.in);
 
-    
-        int[][] matriz = new int[3][3];
-        
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese los valores para la matriz 3x3:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print("Ingrese el valor en la posición [" + i + "][" + j + "]: ");
-                matriz[i][j] = scanner.nextInt();
+    public class MultiplosDeCinco {
+        public static void main(String[] args) {
+            // Iterar sobre los primeros 50 múltiplos de 5
+            for (int i = 1; i <= 50; i++) {
+                // Calcular el múltiplo de 5
+                int multiplo = i * 5;
+                // Mostrar el resultado
+                System.out.println("Múltiplo " + i + " de 5: " + multiplo);
             }
         }
-
-        System.out.println("La matriz original es:");
-        mostrarMatriz(matriz);
-
-        int[][] transpuesta = calcularTranspuesta(matriz);
-        System.out.println("La transpuesta de la matriz es:");
-        mostrarMatriz(transpuesta);
-    }
-
-    public static void mostrarMatriz(int[][] matriz) {
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print(matriz[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static int[][] calcularTranspuesta(int[][] matriz) {
-        int[][] transpuesta = new int[matriz[0].length][matriz.length];
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                transpuesta[j][i] = matriz[i][j];
-            }
-        }
-        return transpuesta;
     }
 }
